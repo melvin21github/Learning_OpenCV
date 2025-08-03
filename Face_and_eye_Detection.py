28287cd6f375819d0271098cfa75f6f17cd719e2
melvin21github <melvintk09@gmail.com>
2025-08-03 17:58:55 +0200

    just changed scale factor fot the eye detection

--- Face_and_eye_Detection.py
+++ Face_and_eye_Detection.py
@@ -22,7 +22,7 @@ for (x, y, w, h) in faces:
     roi_gray = gray[y:y+h, x:x+w]
     roi_color = img[y:y+h, x:x+w]
     # Detect eyes within the face region
-    eyes = eye_cascade.detectMultiScale(roi_gray, scaleFactor=1.1, minNeighbors=5)
+    eyes = eye_cascade.detectMultiScale(roi_gray, scaleFactor=1.3, minNeighbors=5)
     for (ex, ey, ew, eh) in eyes:
         cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
 
